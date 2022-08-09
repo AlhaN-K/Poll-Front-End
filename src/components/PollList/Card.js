aec9b2a98a77c0b50fb33741f538098e39c4accb
Alhan.K <alhan.kaypour@gmail.com>
2022-08-09 14:48:02 +0430

    Added delete button to PollList Card

--- src/components/PollList/Card.js
+++ src/components/PollList/Card.js
@@ -1,25 +1,78 @@
 import "./PollList.css";
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { CardActionArea } from "@mui/material";
 
+const options = ["Delete"];
+
+const ITEM_HEIGHT = 48;
 const PollCard = ({ onRemove, pollNumber }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className="card-container">
         <Card className="cardStyle">
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-controls={open ? "long-menu" : undefined}
+              aria-expanded={open ? "true" : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="long-menu"
+              MenuListProps={{
+                "aria-labelledby": "long-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                  width: "20ch",
+                },
+              }}
+            >
+              {options.map((option) => (
+                <MenuItem
+                  key={option}
+                  selected={option === "Delete"}
+                  onClick={onRemove}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
           <CardActionArea>
             <CardContent>
               <Typography
                 gutterBottom
                 variant="h5"
                 component="div"
-                color="crimson"
+                color="plum"
               >
-                * Poll {pollNumber} *
+                Poll {pollNumber}:
               </Typography>
+
               <Typography gutterBottom variant="h6" component="div">
                 Title:
               </Typography>
@@ -31,12 +84,6 @@ const PollCard = ({ onRemove, pollNumber }) => {
               </Typography>
             </CardContent>
           </CardActionArea>
-          <Button size="small" color="secondary">
-            Edit
-          </Button>{" "}
-          <Button size="small" color="secondary" onClick={onRemove}>
-            Delete
-          </Button>
         </Card>
       </div>
     </>
